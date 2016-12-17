6682df96818d551d60031ca8dd6bd16ea470a2c9
Craig Turner <cragster33@hotmail.com>
2016-12-17 23:33:58 +0000

    Change to dashboard - added lists

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,6 +8,30 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  items = [
+      'Pokémon Yellow',
+      'Super Metroid',
+      'Mega Man X',
+      'The Legend of Zelda',
+      'Pac-Man',
+      'Super Mario World',
+      'Street Fighter II',
+      'Half Life',
+      'Final Fantasy VII',
+      'Star Fox',
+      'Tetris',
+      'Donkey Kong III',
+      'GoldenEye 007',
+      'Doom',
+      'Fallout',
+      'GTA',
+      'Halo'
+    ];
+
+  itemSelected(item: string) {
+    console.log("Selected Item", item);
+  }
+
   constructor(public navCtrl: NavController) {
 
   }
